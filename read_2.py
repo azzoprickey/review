4ad25d76508047408f9ad35996754a13edd1b157
azzoprickey <starg9042@gmail.com>
2022-02-22 20:04:05 +0800

    word_count

---
+++ read_2.py
@@ -0,0 +1,32 @@
+data = []
+count = 0
+with open('reviews.txt','r') as f:
+	for line in f:
+		data.append(line)
+		count += 1
+		if count % 1000 == 0:
+			print(len(data))
+print('檔案讀取完了,總共有',len(data),'筆資料')
+
+
+wc = {}
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1 
+		else:
+			wc[word] = 1
+for word in wc:
+	if wc[word] >1000000:
+		print(word,wc[word])
+	
+while True:
+	word = input('請問你想查什麼字:')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word,'出現的次數為',wc[word])
+	else:
+		print('沒有這個字')
+print('感謝使用')
